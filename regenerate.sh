eec50e75605695511c10e97c64b8c7a6b19cd0cf
Uwe Hoffmann <534011+uwedeportivo@users.noreply.github.com>
2024-09-14 12:11:14 -0700

    sequences and series

--- regenerate.sh
+++ regenerate.sh
@@ -3,7 +3,7 @@
 NOTE_DIRS='airplane_seating bernstein bridge burnside_polya catvsdog completeness counting divisible_by_three'
 NOTE_DITS+=' dutch_national_flag enigma fibolucci grasshopper groovy hammingcode'
 NOTE_DIRS+=' maxsum noconsecutiveints paying_dollar penn_teller pointsoncircle prison_cells'
-NOTE_DIRS+=' sequences threeplussqrtfive trailingzeros twelvecoins two_algebraic_delights twodecks while_a'
+NOTE_DIRS+=' sequences series threeplussqrtfive trailingzeros twelvecoins two_algebraic_delights twodecks while_a'
 
 
 for NOTE_DIR in $NOTE_DIRS
