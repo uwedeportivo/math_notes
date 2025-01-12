0bdbfe07f9c4756006221c44517574031cb1f42d
Uwe Hoffmann <534011+uwedeportivo@users.noreply.github.com>
2025-01-12 12:14:25 -0800

    nth root 2

--- copy.sh
+++ copy.sh
@@ -2,7 +2,7 @@
 
 NOTE_DIRS='airplane_seating bernstein bridge burnside_polya catvsdog completeness counting divisible_by_three'
 NOTE_DITS+=' dutch_national_flag enigma fibolucci grasshopper groovy hammingcode'
-NOTE_DIRS+=' maxsum noconsecutiveints paying_dollar penn_teller pointsoncircle prison_cells'
+NOTE_DIRS+=' maxsum noconsecutiveints nthroot paying_dollar penn_teller pointsoncircle prison_cells'
 NOTE_DIRS+=' sequences series threeplussqrtfive trailingzeros twelvecoins two_algebraic_delights twodecks while_a'
 
 
